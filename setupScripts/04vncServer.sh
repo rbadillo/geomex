6443d31e4731bc6df7b0c03012dbe4891429ee78
rbadillo <beto_best@hotmail.com>
2013-08-04 23:23:52 -0500

    Update 04vncServer.sh

--- setupScripts/04vncServer.sh
+++ setupScripts/04vncServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gsettings set org.gnome.Vino enabled true
 gsettings set org.gnome.Vino prompt-enabled false
-/usr/lib/vino/vino-server
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password 'RXN0YVRyaXZpYWxWbmMh'
