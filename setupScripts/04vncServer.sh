009d86e98165c1fd97a9f331fca2c329c0b1669a
rbadillo <beto_best@hotmail.com>
2013-08-04 23:45:44 -0500

    Delete 04vncServer.sh

--- setupScripts/04vncServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gsettings set org.gnome.Vino enabled true
-gsettings set org.gnome.Vino prompt-enabled false
-gsettings set org.gnome.Vino authentication-methods "['vnc']"
-gsettings set org.gnome.Vino vnc-password 'RXN0YVRyaXZpYWxWbmMh'
