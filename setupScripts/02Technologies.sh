aa115a0b69128b2a747c92b3098043ce93d04410
Alfonso Ayala <poncho@ubuntu.(none)>
2013-08-04 19:20:02 -0700

    Setup Scripts

---
+++ setupScripts/02Technologies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install -y git-core
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update -y
+sudo apt-get install -y nodejs
+sudo apt-get install -y rabbitmq-server
+sudo apt-get install -y mysql-server
+sudo apt-get install -y openssh-server
+gsettings set org.gnome.Vino enabled true
+gsettings set org.gnome.Vino prompt-enabled false
+/usr/lib/vino/vino-server
