c74c4e5c5c6f23c86d24abf4193819a9b4b642ef
rbadillo <beto_best@hotmail.com>
2013-08-05 21:08:04 -0500

    Corrigiendo el nombre de la carpeta

---
+++ setupScripts2/04VncServer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gsettings set org.gnome.Vino enabled true
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password 'RXN0YVRyaXZpYWxWbmMh'
