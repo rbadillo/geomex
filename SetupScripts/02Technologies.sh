1bd6c7caf5cbe12ec4345ab72c983d680e892c54
rbadillo <beto_best@hotmail.com>
2013-08-05 21:10:10 -0500

    carpeta corregida

---
+++ SetupScripts/02Technologies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update -y
+sudo apt-get install -y nodejs=0.10.15-1chl1~precise1
+sudo apt-get install -y rabbitmq-server
+echo mysql-server-5.5 mysql-server/root_password password EstaTrivialDb! | debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password EstaTrivialDb! | debconf-set-selections
+sudo apt-get install -y mysql-server
+sudo apt-get install -y openssh-server
+sudo apt-get install -y redis-server
