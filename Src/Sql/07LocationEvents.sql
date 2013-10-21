b4cb40423ce9346f681e73e6d48965db4db2d929
rbadillo <beto_best@hotmail.com>
2013-10-20 19:46:18 -0500

    Update 07LocationEvents.sql
    Adding ClientId FK.

--- Src/Sql/07LocationEvents.sql
+++ Src/Sql/07LocationEvents.sql
@@ -1,15 +1,22 @@
 CREATE  TABLE IF NOT EXISTS `geomex`.`LocationEvents` (
   `Id` INT NOT NULL AUTO_INCREMENT ,
   `UserId` INT NOT NULL ,
+  `ClientId` INT NOT NULL ,
   `LocationId` INT NOT NULL ,
   `Event` VARCHAR(255) NOT NULL ,
   `TimeCreated` TIMESTAMP NOT NULL ,
   PRIMARY KEY (`Id`) ,
   INDEX `UserId` (`UserId` ASC) ,
+  INDEX `ClientId` (`ClientId` ASC) ,
   INDEX `LocationId` (`LocationId` ASC) ,
   CONSTRAINT `UserId_LocationEvents`
     FOREIGN KEY (`UserId` )
     REFERENCES `geomex`.`Users` (`UserId` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `ClientId_LocationEvents`
+    FOREIGN KEY (`ClientId` )
+    REFERENCES `geomex`.`Clients` (`ClientId` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `LocationId_LocationEvents`
