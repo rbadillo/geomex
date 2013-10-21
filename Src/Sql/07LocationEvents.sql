5924fb1785c86c912052a4123a5cb59243671ed2
rbadillo <beto_best@hotmail.com>
2013-10-20 19:19:29 -0500

    Create 07LocationEvents.sql
    Adding new Table

---
+++ Src/Sql/07LocationEvents.sql
@@ -0,0 +1,19 @@
+CREATE  TABLE IF NOT EXISTS `geomex`.`LocationEvents` (
+  `Id` INT NOT NULL AUTO_INCREMENT ,
+  `UserId` INT NOT NULL ,
+  `LocationId` INT NOT NULL ,
+  `Event` VARCHAR(255) NOT NULL ,
+  `TimeCreated` TIMESTAMP NOT NULL ,
+  PRIMARY KEY (`Id`) ,
+  INDEX `UserId` (`UserId` ASC) ,
+  INDEX `LocationId` (`LocationId` ASC) ,
+  CONSTRAINT `UserId_LocationEvents`
+    FOREIGN KEY (`UserId` )
+    REFERENCES `geomex`.`Users` (`UserId` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `LocationId_LocationEvents`
+    FOREIGN KEY (`LocationId` )
+    REFERENCES `geomex`.`Locations` (`LocationId` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
