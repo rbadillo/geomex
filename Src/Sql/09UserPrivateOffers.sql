3f24bdb683846b5fd3df80500aef58cb08cac912
rbadillo <beto_best@hotmail.com>
2013-10-23 19:49:17 -0500

    Add ClientId Foreign Key.

--- Src/Sql/09UserPrivateOffers.sql
+++ Src/Sql/09UserPrivateOffers.sql
@@ -1,13 +1,20 @@
 CREATE  TABLE `geomex`.`UserPrivateOffers` (
   `Id` INT NOT NULL AUTO_INCREMENT ,
+  `ClientId` INT NOT NULL ,
   `UserId` INT NOT NULL ,
   `OfferId` INT NOT NULL ,
   `StartDate` TIMESTAMP NOT NULL ,
   `EndDate` TIMESTAMP NOT NULL ,
   `TimeCreated` TIMESTAMP NOT NULL ,
   PRIMARY KEY (`Id`) ,
+  INDEX `ClientIdPrivateOffers` (`ClientId` ASC) ,
   INDEX `UserIdPrivateOffers` (`UserId` ASC) ,
   INDEX `OfferIdPrivateOffers` (`OfferId` ASC) ,
+  CONSTRAINT `ClientIdPrivateOffers`
+    FOREIGN KEY (`ClientId` )
+    REFERENCES `geomex`.`Clients` (`ClientId` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `UserIdPrivateOffers`
     FOREIGN KEY (`UserId` )
     REFERENCES `geomex`.`Users` (`UserId` )
