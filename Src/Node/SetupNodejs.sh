2d37766f5c3459fa44ed0e144c409c5385abc53a
rbadillo <beto_best@hotmail.com>
2013-11-23 12:33:38 -0600

    Update APN version that include shutdown function

--- Src/Node/SetupNodejs.sh
+++ Src/Node/SetupNodejs.sh
@@ -7,7 +7,7 @@ sudo npm install express@3.3.4
 sudo npm install amqp@0.1.7
 sudo npm install moment@2.1.0
 sudo npm install mysql@2.0.0-alpha7
-sudo npm install apn@1.3.8
+sudo npm install apn@1.4.3
 sudo npm install winston@0.7.2
 sudo npm install forever@0.10.8 -g
 sudo npm install node-gcm@0.9.6
