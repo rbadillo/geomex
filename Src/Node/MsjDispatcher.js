30d7c1de9d4452e0cc0fdc544cc2d1b01aec7e6f
rbadillo <beto_best@hotmail.com>
2013-11-23 21:28:34 -0600

    Adding Console Statements For Tracking Bugs

--- Src/Node/MsjDispatcher.js
+++ Src/Node/MsjDispatcher.js
@@ -5,12 +5,14 @@ var connection = amqp.createConnection();
 
 // Wait for connection to become established.
 connection.on('ready', function () {
+  console.log("MsjDispatcher - Connected to RabbitMQ")
   // Use the default 'amq.topic' exchange
   var options = { autoDelete: false, durable: true };
   var q = connection.queue('PushMessages',options);
 
           // Receive messages
           q.subscribe(function (message) {
+            console.log("MsjDispatcher - Got Message From RabbitMQ")
             // Print messages to stdout
             
             //console.log(message.Users.iOS);
@@ -21,7 +23,10 @@ connection.on('ready', function () {
                     //console.log(iOSPhones);
 
                     if(iOSPhones.length > 0){
+                      console.log("MsjDispatcher - Sending Push To iOS Devices")
                       Ios.PushMessage(message.Message,iOSPhones,message.ClientName);
+                    }else{
+                      console.log("MsjDispatcher - Not Active iOS Users To Send Push Notification")
                     }
                 }
             
@@ -30,10 +35,16 @@ connection.on('ready', function () {
                     //console.log(AndroidPhones);
 
                     if(AndroidPhones.length > 0){
+                      console.log("MsjDispatcher - Sending Push To Android Devices")
                       Android.PushMessage(message.Message,AndroidPhones,message.ClientName);
+                    }else{
+                      console.log("MsjDispatcher - Not Active Android Users To Send Push Notification")
                     }
               }
 
+          }else{
+            console.log("MsjDispatcher - Got Message Without Users Property From RabbitMQ")
+            console.log(message)
           }
       });
       
