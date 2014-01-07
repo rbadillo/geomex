809b71707dd29038d2614829f826f8abdc774a14
rbadillo <beto_best@hotmail.com>
2014-01-06 22:16:11 -0600

    Update 12AppEvents.sql

--- Src/Sql/12AppEvents.sql
+++ Src/Sql/12AppEvents.sql
@@ -1,11 +1,11 @@
-CREATE  TABLE `geomex`.`AppEvents` (
+CREATE  TABLE `geomex`.`new_table` (
   `Id` INT NOT NULL AUTO_INCREMENT ,
   `UserId` INT NOT NULL ,
   `Event` VARCHAR(100) NOT NULL ,
   `TimeCreated` TIMESTAMP NULL ,
   PRIMARY KEY (`Id`) ,
-  INDEX `UserId_AppEvents` (`UserId` ASC) ,
-  CONSTRAINT `UserId`
+  INDEX `UserId_AppEvent` (`UserId` ASC) ,
+  CONSTRAINT `UserId_AppEvent`
     FOREIGN KEY (`UserId` )
     REFERENCES `geomex`.`Users` (`UserId` )
     ON DELETE NO ACTION
