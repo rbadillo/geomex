68300ac5cb4f2c86c26d8d77a3b9d39c07f41c4f
rbadillo <beto_best@hotmail.com>
2014-01-07 21:19:23 -0600

    Adding Latitude and Longitude

--- Src/Sql/12AppEvents.sql
+++ Src/Sql/12AppEvents.sql
@@ -2,6 +2,8 @@ CREATE  TABLE `geomex`.`AppEvents` (
   `Id` INT NOT NULL AUTO_INCREMENT ,
   `UserId` INT NOT NULL ,
   `Event` VARCHAR(100) NOT NULL ,
+  `Latitude` DECIMAL(10,6) NULL DEFAULT NULL,
+  `Longitude` DECIMAL(10,6) NULL DEFAULT NULL,
   `TimeCreated` TIMESTAMP NULL ,
   PRIMARY KEY (`Id`) ,
   INDEX `UserId_AppEvent` (`UserId` ASC) ,
