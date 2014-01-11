0344459cf8d2c8979fef359546969717a1dac54e
rbadillo <beto_best@hotmail.com>
2014-01-11 16:36:37 -0600

    Update 02ClientsTable.sql

--- Src/Sql/02ClientsTable.sql
+++ Src/Sql/02ClientsTable.sql
@@ -2,6 +2,13 @@ CREATE  TABLE IF NOT EXISTS `geomex`.`Clients` (
   `ClientId` INT NOT NULL AUTO_INCREMENT ,
   `Name` VARCHAR(255) NOT NULL ,
   `Logo` VARCHAR(255) NULL ,
-  `IsActive` TINYINT NOT NULL DEFAULT 1
+  `IsActive` TINYINT NOT NULL DEFAULT 1,
+  `_Created` DATETIME NOT NULL ,
+  `_Updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
   PRIMARY KEY (`ClientId`) );
 
+DROP TRIGGER IF EXISTS `geomex`.`Clients_Datetime_Created`;
+
+CREATE TRIGGER `geomex`.`Clients_Datetime_Created` BEFORE INSERT ON `geomex`.`Clients` 
+FOR EACH ROW
+SET NEW._Created = NOW();
