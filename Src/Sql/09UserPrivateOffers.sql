0495909a983eec8a3d7f5970869a4d96c402577f
rbadillo <beto_best@hotmail.com>
2014-01-11 17:00:59 -0600

    Adding Trigger for UserPrivateOffers

--- Src/Sql/09UserPrivateOffers.sql
+++ Src/Sql/09UserPrivateOffers.sql
@@ -6,6 +6,8 @@ CREATE  TABLE `geomex`.`UserPrivateOffers` (
   `StartDate` DATETIME NOT NULL ,
   `EndDate` DATETIME NOT NULL ,
   `TimeCreated` DATETIME NOT NULL ,
+  `_Created` DATETIME NOT NULL ,
+  `_Updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
   PRIMARY KEY (`Id`) ,
   INDEX `ClientIdPrivateOffers` (`ClientId` ASC) ,
   INDEX `UserIdPrivateOffers` (`UserId` ASC) ,
@@ -25,3 +27,9 @@ CREATE  TABLE `geomex`.`UserPrivateOffers` (
     REFERENCES `geomex`.`Offers` (`OfferId` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
+DROP TRIGGER IF EXISTS `geomex`.`UserPrivateOffers_Datetime_Created`;
+
+CREATE TRIGGER `geomex`.`UserPrivateOffers_Datetime_Created` BEFORE INSERT ON `geomex`.`UserPrivateOffers` 
+FOR EACH ROW
+SET NEW._Created = NOW();
