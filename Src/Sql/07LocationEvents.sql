9d49f995211e06402fecbe05aab3d841b578ef28
rbadillo <beto_best@hotmail.com>
2014-01-11 15:53:27 -0600

    Update 07LocationEvents.sql

--- Src/Sql/07LocationEvents.sql
+++ Src/Sql/07LocationEvents.sql
@@ -7,7 +7,7 @@ CREATE  TABLE IF NOT EXISTS `geomex`.`LocationEvents` (
   `Event` VARCHAR(255) NOT NULL ,
   `Latitude` DECIMAL(10,6) NULL DEFAULT NULL,
   `Longitude` DECIMAL(10,6) NULL DEFAULT NULL, 
-  `TimeCreated` TIMESTAMP NULL ,
+  `TimeCreated` DATETIME NULL ,
   PRIMARY KEY (`Id`) ,
   INDEX `ClientId` (`ClientId` ASC) ,
   INDEX `LocationId` (`LocationId` ASC) ,
