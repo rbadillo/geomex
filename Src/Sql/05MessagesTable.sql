029fbca380a4b5106fe29eaef63076841c3a80c9
rbadillo <beto_best@hotmail.com>
2014-01-11 16:51:33 -0600

    Trigger for Messages Table.

--- Src/Sql/05MessagesTable.sql
+++ Src/Sql/05MessagesTable.sql
@@ -5,6 +5,8 @@ CREATE  TABLE IF NOT EXISTS `geomex`.`Messages` (
   `ClientId` INT NOT NULL ,
   `Visibility` VARCHAR(100) NOT NULL ,
   `TimeCreated` DATETIME NOT NULL ,
+  `_Created` DATETIME NOT NULL ,
+  `_Updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
   PRIMARY KEY (`MessageId`) ,
   INDEX `LocationId_idx` (`LocationId` ASC) ,
   INDEX `ClientId_idx` (`ClientId` ASC) ,
@@ -19,3 +21,8 @@ CREATE  TABLE IF NOT EXISTS `geomex`.`Messages` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
+DROP TRIGGER IF EXISTS `geomex`.`Messages_Datetime_Created`;
+
+CREATE TRIGGER `geomex`.`Messages_Datetime_Created` BEFORE INSERT ON `geomex`.`Messages` 
+FOR EACH ROW
+SET NEW._Created = NOW();
