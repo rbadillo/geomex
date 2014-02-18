0b4ef4b5f06744fd07a01d640ff6fe754e13250e
rbadillo <beto_best@hotmail.com>
2014-02-17 22:25:05 -0600

    Schema Ready for V2

---
+++ Src/Sql/05Offers.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS `geomex`.`Offers`;
+
+CREATE TABLE IF NOT EXISTS `geomex`.`Offers` (
+  `OfferId` INT NOT NULL AUTO_INCREMENT ,
+  `ClientId` INT NOT NULL ,
+  `Name` VARCHAR(255) NOT NULL ,
+  `Title` VARCHAR(255) NOT NULL ,
+  `Subtitle` VARCHAR(255) NULL ,
+  `Code` VARCHAR(255) NULL ,
+  `Instructions` VARCHAR(1024) NULL ,
+  `Disclaimer` VARCHAR(1024) NULL ,
+  `PublishedDate` DATETIME NOT NULL ,
+  `StartDate` DATETIME NOT NULL ,
+  `EndDate` DATETIME NOT NULL ,
+  `IsActive` TINYINT NOT NULL DEFAULT 1 ,
+  `Priority` INT NULL ,
+  `ActualRedemption` INT NULL ,
+  `TotalRedemption` INT NULL ,
+  `MultiUse` TINYINT NOT NULL DEFAULT 0 ,
+  `Visibility` VARCHAR(45) NOT NULL ,
+  `DynamicRedemptionMinutes` INT NULL ,
+  `PrimaryImage` VARCHAR(255) NULL ,
+  `SecondaryImage` VARCHAR(255) NULL ,
+  `_Created` DATETIME NOT NULL ,
+  `_Updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`OfferId`) ,
+  INDEX `ClientIdOffers` (`ClientId` ASC) ,
+  CONSTRAINT `ClientIdOffers`
+    FOREIGN KEY (`ClientId` )
+    REFERENCES `geomex`.`Clients` (`ClientId` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+DROP TRIGGER IF EXISTS `geomex`.`Offers_Datetime_Created`;
+
+CREATE TRIGGER `geomex`.`Offers_Datetime_Created` BEFORE INSERT ON `geomex`.`Offers` 
+FOR EACH ROW
+SET NEW._Created = NOW();
--- Src/Sql/SetupDatabase.sh
+++ Src/Sql/SetupDatabase.sh
@@ -3,10 +3,10 @@ mysql -u root -pEstaTrivialDb! < 01CreatingDatabase.sql;
 mysql -u root -pEstaTrivialDb! < 02ClientsTable.sql;
 mysql -u root -pEstaTrivialDb! < 03LocationsTable.sql;
 mysql -u root -pEstaTrivialDb! < 04UsersTable.sql;
-mysql -u root -pEstaTrivialDb! < 05MessagesTable.sql;
-mysql -u root -pEstaTrivialDb! < 06SentMessages.sql;
-mysql -u root -pEstaTrivialDb! < 07LocationEvents.sql
-mysql -u root -pEstaTrivialDb! < 08Offers.sql
+mysql -u root -pEstaTrivialDb! < 05Offers.sql;
+mysql -u root -pEstaTrivialDb! < 06MessagesTable.sql;
+mysql -u root -pEstaTrivialDb! < 07SentMessages.sql
+mysql -u root -pEstaTrivialDb! < 08LocationEvents.sql
 mysql -u root -pEstaTrivialDb! < 09UserPrivateOffers.sql
 mysql -u root -pEstaTrivialDb! < 10OfferRedemption.sql
 mysql -u root -pEstaTrivialDb! < 11OfferEvents.sql
