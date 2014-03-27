c68d5f33f7d981b0a7f772f9a787ec28c79943b9
rbadillo <beto_best@hotmail.com>
2014-03-26 20:48:28 -0500

    Adding Python Mysql Module

--- SetupScripts/03Utils.sh
+++ SetupScripts/03Utils.sh
@@ -7,3 +7,4 @@ sudo apt-get install -y mysql-workbench
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
 sudo apt-get update 
 sudo apt-get install -y sublime-text
+sudo apt-get install -y python-mysqldb
