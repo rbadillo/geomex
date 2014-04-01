76cda49f5196a022c5e13d80dc9e159626e6b05d
rbadillo <beto_best@hotmail.com>
2014-03-31 23:11:25 -0500

    Fix Log Directory

--- Utilities/NearServer.sh
+++ Utilities/NearServer.sh
@@ -1,6 +1,6 @@
-echo 'EstaTrivialBox!' | sudo -S /usr/bin/forever start -l /home/geomex/Near/current/log/RouterServer.log /home/geomex/Near/current/Src/Node/RouterServer.js
-/usr/bin/forever start -l /home/geomex/Near/current/log/RegisterServer.log /home/geomex/Near/current/Src/Node/RegisterServer.js
-/usr/bin/forever start -l /home/geomex/Near/current/log/QueueWorker.log /home/geomex/Near/current/Src/Node/QueueWorker.js
-/usr/bin/forever start -l /home/geomex/Near/current/log/MsjDispatcher.log /home/geomex/Near/current/Src/Node/MsjDispatcher.js
-/usr/bin/forever start -l /home/geomex/Near/current/log/FeatureServer.log /home/geomex/Near/current/Src/Node/FeatureServer.js
-/usr/bin/forever start -l /home/geomex/Near/current/log/OfferServer.log /home/geomex/Near/current/Src/Node/OfferServer.js
+echo 'EstaTrivialBox!' | sudo -S /usr/bin/forever start -l /home/geomex/Logs/RouterServer.log /home/geomex/Near/current/Src/Node/RouterServer.js
+/usr/bin/forever start -l /home/geomex/Logs/RegisterServer.log /home/geomex/Near/current/Src/Node/RegisterServer.js
+/usr/bin/forever start -l /home/geomex/Logs/QueueWorker.log /home/geomex/Near/current/Src/Node/QueueWorker.js
+/usr/bin/forever start -l /home/geomex/Logs/MsjDispatcher.log /home/geomex/Near/current/Src/Node/MsjDispatcher.js
+/usr/bin/forever start -l /home/geomex/Logs/FeatureServer.log /home/geomex/Near/current/Src/Node/FeatureServer.js
+/usr/bin/forever start -l /home/geomex/Logs/OfferServer.log /home/geomex/Near/current/Src/Node/OfferServer.js
