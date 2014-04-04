6e0bb3e3ac84ab073a95102ebb390aab270113eb
geomex <geomex@ubuntu.(none)>
2014-04-04 01:34:38 -0500

    Fix para el Path de Forever

--- Utilities/NearServer.sh
+++ Utilities/NearServer.sh
@@ -1,6 +1,6 @@
-echo 'EstaTrivialBox!' | sudo -S /usr/bin/forever start -a -l /home/geomex/Logs/RouterServer.log /home/geomex/Near/current/Src/Node/RouterServer.js
-/usr/bin/forever start -a -l /home/geomex/Logs/RegisterServer.log /home/geomex/Near/current/Src/Node/RegisterServer.js
-/usr/bin/forever start -a -l /home/geomex/Logs/QueueWorker.log /home/geomex/Near/current/Src/Node/QueueWorker.js
-/usr/bin/forever start -a -l /home/geomex/Logs/MsjDispatcher.log /home/geomex/Near/current/Src/Node/MsjDispatcher.js
-/usr/bin/forever start -a -l /home/geomex/Logs/FeatureServer.log /home/geomex/Near/current/Src/Node/FeatureServer.js
-/usr/bin/forever start -a -l /home/geomex/Logs/OfferServer.log /home/geomex/Near/current/Src/Node/OfferServer.js
+echo 'EstaTrivialBox!' | sudo -S forever start -a -l /home/geomex/Logs/RouterServer.log /home/geomex/Near/current/Src/Node/RouterServer.js
+forever start -a -l /home/geomex/Logs/RegisterServer.log /home/geomex/Near/current/Src/Node/RegisterServer.js
+forever start -a -l /home/geomex/Logs/QueueWorker.log /home/geomex/Near/current/Src/Node/QueueWorker.js
+forever start -a -l /home/geomex/Logs/MsjDispatcher.log /home/geomex/Near/current/Src/Node/MsjDispatcher.js
+forever start -a -l /home/geomex/Logs/FeatureServer.log /home/geomex/Near/current/Src/Node/FeatureServer.js
+forever start -a -l /home/geomex/Logs/OfferServer.log /home/geomex/Near/current/Src/Node/OfferServer.js
