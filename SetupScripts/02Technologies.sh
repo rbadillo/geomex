814b12f118aaaad51ae945d03e7190c665e6cbae
rbadillo <beto_best@hotmail.com>
2014-04-04 21:04:48 -0500

    Change to install the latest nodejs version

--- SetupScripts/02Technologies.sh
+++ SetupScripts/02Technologies.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update -y
-sudo apt-get install -y nodejs=0.10.15-1chl1~precise1
+sudo apt-get install -y nodejs
 sudo apt-get install -y rabbitmq-server
 echo mysql-server-5.5 mysql-server/root_password password EstaTrivialDb! | debconf-set-selections
 echo mysql-server-5.5 mysql-server/root_password_again password EstaTrivialDb! | debconf-set-selections
