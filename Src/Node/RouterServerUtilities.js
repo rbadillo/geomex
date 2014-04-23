7394fa814f633d3d4e0bdd33ae117b260d04828e
geomex <geomex@ubuntu.(none)>
2014-04-22 21:19:52 -0500

    Fix para query parameters y Agregando HealthCheck Script

--- Src/Node/RouterServerUtilities.js
+++ Src/Node/RouterServerUtilities.js
@@ -2,55 +2,55 @@ var request = require('request');
 
 //FeatureServer
 exports.GetAllActiveClients = function GetAllActiveClients(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.GetClosestLocations = function GetClosestLocations(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.GetFriends = function GetFriends(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.post('http://127.0.0.1:4000'+RedirectUrl,{form:req.body}).pipe(res)
 	console.log()
 }
 
 exports.GetFriendActivity = function GetFriendActivity(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.IsLocationActive = function IsLocationActive(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.GetUnreadMessages = function GetUnreadMessages(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.GetMessages = function GetMessages(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.ReadMessage = function ReadMessage(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.ShowGeoMessage = function ShowGeoMessage(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:4000'+RedirectUrl).pipe(res)
 	console.log()
 }
@@ -58,19 +58,19 @@ exports.ShowGeoMessage = function ShowGeoMessage(req,res){
 
 //OfferServer
 exports.Offers = function Offers(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:5000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.SingleOffer = function SingleOffer(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:5000'+RedirectUrl).pipe(res)
 	console.log()
 }
 
 exports.Redeem = function Redeem(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.get('http://127.0.0.1:5000'+RedirectUrl).pipe(res)
 	console.log()
 }
@@ -79,13 +79,13 @@ exports.Redeem = function Redeem(req,res){
 
 //RegisterServer
 exports.Register = function Register(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.post('http://127.0.0.1:8000'+RedirectUrl,{form:req.body}).pipe(res)
 	console.log()
 }
 
 exports.GeoEvent = function GeoEvent(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.post('http://127.0.0.1:8000'+RedirectUrl,{form:req.body}).pipe(res)
 	console.log()
 }
@@ -93,7 +93,7 @@ exports.GeoEvent = function GeoEvent(req,res){
 
 //QueueWorker
 exports.SendMessage = function SendMessage(req,res){
-	RedirectUrl=req.path
+	RedirectUrl=req.originalUrl
 	request.post('http://127.0.0.1:7000'+RedirectUrl,{form:req.body}).pipe(res)
 	console.log()
 }
---
+++ Utilities/HealthCheck.py
@@ -0,0 +1,76 @@
+import requests
+import smtplib
+import datetime
+
+Timeout=5
+
+print ""
+print "Start: " +str(datetime.datetime.now())
+
+try:
+	r = requests.get('http://near.noip.me/517218456/GetAllActiveClients',timeout=Timeout)
+	Code=r.status_code
+	ResponseTime=r.elapsed
+	ResponseTime=str(ResponseTime).split(".")
+	ResponseTime=ResponseTime[1][:-3]
+	print "Code: " +str(Code)
+	print "Response Time: " +str(ResponseTime) +" miliseconds"
+	if(Code!=200):
+		print "Sending Email - Health Check HTTP CODE Error Alert"
+		fromaddr = 'descubrenear@gmail.com'
+		toaddrs  = 'beto_best@hotmail.com'
+		subject="Near - Health Check HTTP CODE Error Alert"
+		body = "Health Check HTTP CODE Error: " +str(Code)
+		message = 'Subject: %s\n\n%s' % (subject, body)
+
+		# Credentials (if needed)
+		username = 'descubrenear'
+		password = 'badillogonzalez'
+
+		# The actual mail send
+		server = smtplib.SMTP('smtp.gmail.com:587')
+		server.starttls()
+		server.login(username,password)
+		server.sendmail(fromaddr, toaddrs, message)
+		server.quit()
+
+except requests.exceptions.Timeout:
+	print "Sending Email - Health Check Timeout Alert"
+	fromaddr = 'descubrenear@gmail.com'
+	toaddrs  = 'beto_best@hotmail.com'
+	subject="Near - Health Check Timeout Alert"
+	body = "Health Check Exceeded Timeout of " +str(Timeout) +" seconds"
+	message = 'Subject: %s\n\n%s' % (subject, body)
+
+	# Credentials (if needed)
+	username = 'descubrenear'
+	password = 'badillogonzalez'
+
+	# The actual mail send
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.starttls()
+	server.login(username,password)
+	server.sendmail(fromaddr, toaddrs, message)
+	server.quit()
+except requests.exceptions.ConnectionError:
+	print "Sending Email - Health Check ConnectionError Alert"
+	fromaddr = 'descubrenear@gmail.com'
+	toaddrs  = 'beto_best@hotmail.com'
+	subject="Near - Health Check ConnectionError Alert"
+	body = "Health Check ConnectionError"
+	message = 'Subject: %s\n\n%s' % (subject, body)
+
+	# Credentials (if needed)
+	username = 'descubrenear'
+	password = 'badillogonzalez'
+
+	# The actual mail send
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.starttls()
+	server.login(username,password)
+	server.sendmail(fromaddr, toaddrs, message)
+	server.quit()
+
+print "End: " +str(datetime.datetime.now())
+print ""
+
