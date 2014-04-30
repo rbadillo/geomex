39e5fc66270c6e2062bdf6e2691d8f4a7f436a04
rbadillo <beto_best@hotmail.com>
2014-04-29 20:39:48 -0500

    Agregando columna de ClientId para nuevo AppEvent.

--- Src/Sql/12AppEvents.sql
+++ Src/Sql/12AppEvents.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS `geomex`.`AppEvents`;
 CREATE TABLE IF NOT EXISTS `geomex`.`AppEvents` (
   `Id` INT NOT NULL AUTO_INCREMENT ,
   `UserId` BIGINT UNSIGNED NOT NULL ,
+  `ClientId` INT NULL DEFAULT NULL
   `Event` VARCHAR(100) NOT NULL ,
   `Latitude` DECIMAL(10,6) NULL DEFAULT NULL,
   `Longitude` DECIMAL(10,6) NULL DEFAULT NULL,
@@ -11,11 +12,17 @@ CREATE TABLE IF NOT EXISTS `geomex`.`AppEvents` (
   `_Updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`) ,
   INDEX `UserId_AppEvent` (`UserId` ASC) ,
+  INDEX `ClientId_AppEvent_idx` (`ClientId` ASC) ,
   CONSTRAINT `UserId_AppEvent`
     FOREIGN KEY (`UserId` )
     REFERENCES `geomex`.`Users` (`UserId` )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION),
+  CONSTRAINT `ClientId_AppEvent`
+    FOREIGN KEY (`ClientId` )
+    REFERENCES `geomex`.`Clients` (`ClientId` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
 
 DROP TRIGGER IF EXISTS `geomex`.`AppEvents_Datetime_Created`;
 
