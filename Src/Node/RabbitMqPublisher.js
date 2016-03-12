8abeb5c30972ea452cc79dc4fe14e182aef63d1b
geomex <geomex@ubuntu.(none)>
2016-03-12 15:54:33 -0600

    Another Bug in RabbitMQ Library

--- Src/Node/RabbitMqPublisher.js
+++ Src/Node/RabbitMqPublisher.js
@@ -3,13 +3,13 @@ var DAL= require('./Database');
 
 exports.PublishMessage=function PublishMessage(QueueName,OfferId,Devices,MessageTitle,MessageSubtitle,ClientId,ClientName,ClientLogo,SendMessageOnly,callback) {
 
-  var connection = amqp.createConnection({ reconnect: false });
+  var connection = amqp.createConnection({},{ reconnect: false });
 
   connection.on('ready', function () {
       // Prepare to Send Message to RabbitMQ
-      connection.queue(QueueName,function(q){
+      var q = connection.queue(QueueName)
 
-          var msj= {
+      var msj= {
                   "OfferId": OfferId,
                   "Users": Devices,
                   "MessageTitle": MessageTitle,
@@ -20,25 +20,24 @@ exports.PublishMessage=function PublishMessage(QueueName,OfferId,Devices,Message
                   "SendMessageOnly": SendMessageOnly
             }
 
-          console.log("");
-          console.log("Msg Sent to Queue:");
-          console.log("");
-          console.log(msj);
-          console.log("");
+      console.log("");
+      console.log("Msg Sent to Queue:");
+      console.log("");
+      console.log(msj);
+      console.log("");
 
-          connection.publish(QueueName,msj,{},function(err){
-              if(err)
-              {
-                return callback("ERROR - Publishing to RabbitMQ")  
-              }
-              else
-              {
-                  console.log("Message Published Successfully to RabbitMQ");
-                  console.log("");
-                  connection.disconnect()
-                  return callback(null)
-              }     
-          });
+      connection.publish(QueueName,msj,{},function(err){
+          if(err)
+          {
+              return callback("ERROR - Publishing to RabbitMQ")  
+          }
+          else
+          {
+              console.log("Message Published Successfully to RabbitMQ");
+              console.log("");
+              connection.disconnect()
+              return callback(null)
+          }     
       });
   });
 }
