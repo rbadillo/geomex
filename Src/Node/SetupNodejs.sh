364ec642b077177724d4b4027895b28e77bb0e83
geomex <geomex@ubuntu.(none)>
2016-03-15 01:38:15 -0500

    Upgrading Modules

--- Src/Node/SetupNodejs.sh
+++ Src/Node/SetupNodejs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo npm install orm@2.1.0
+sudo npm install orm@2.1.30
 sudo npm install mocha@1.12.0
 sudo npm install redis@0.8.4
 sudo npm install hiredis@0.1.15
 sudo npm install express@3.3.4
 sudo npm install amqp@0.2.4
 sudo npm install moment@2.1.0
-sudo npm install mysql@2.0.0-alpha7
+sudo npm install mysql@2.10.2
 sudo npm install apn@1.4.3
 sudo npm install winston@0.7.2
 sudo npm install forever@0.15.1 -g
