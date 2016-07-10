9dec4d2ef9bc6c5a3ebb78f39c31379d9b0f5e19
Roberto Badillo <rbadillo@descubrenear.com>
2016-07-10 13:56:43 -0500

    Adding Priority to Get Active Clients API

--- Src/Sql/02ClientsTable.sql
+++ Src/Sql/02ClientsTable.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS `geomex`.`Clients` (
   `ClientHexColor` VARCHAR(10) NOT NULL ,
   `IsActive` TINYINT NOT NULL DEFAULT 1,
   `IsGold` TINYINT NOT NULL DEFAULT 0,
+  `Priority` INT DEFAULT NULL,
   `ActiveOffers` INT NOT NULL DEFAULT 0,
   `OfferClosestExpiration` DATETIME NOT NULL ,
   `_Created` DATETIME NOT NULL ,
